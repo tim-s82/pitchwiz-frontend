d8f2712d07a3ec869b366e0b92914987e1edef55
Tim Shapcott <tim.shapcott@gmail.com>
2026-08-20 17:26:39 +0100

    chore: update API proxy target to use VITE_API_URL environment variable

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: import.meta.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       }
